7fa609c9e7d0e669a94734efec778e6349a7a6f0
CHETHANAR11 <chethanasree.r2021it@sece.ac.in>
2024-02-02 21:17:02 +0530

    first commit

---
+++ todo_app/lib/screens/home.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/design/themes.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Container(
+        child: Text('home screen'),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: tdBGColor,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+        Icon(Icons.menu,color: tdBlack,size: 30,),
+        Container(
+          height: 40,
+          width: 40,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset('assets/images/profile.jpeg'),
+          ),
+        )
+      ],
+      
+      ),
+    );
+  }
+}
